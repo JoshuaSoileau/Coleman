637a2c7b6975fa4f6fd1b066d7bf973a8c4f8325
Josh Soileau <joshua.soileau@gmail.com>
2017-02-19 13:15:33 -0500

    Fix mobile menu

--- build/js/see-all.js
+++ build/js/see-all.js
@@ -2,4 +2,9 @@ jQuery(function($) {
     $('.see-more').click(function() {
         $(this).parent().toggleClass('height-auto');
     });
+    $(".menu a").click(function() {
+        $(".state-visible").removeClass("state-visible"),
+        $(".state-fixed").removeClass("state-fixed"),
+        $(".state-opened").removeClass("state-opened")
+    });
 });
