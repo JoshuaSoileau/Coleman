710a7af15839ad61c9d9d4730151b5170e62ee40
Josh Soileau <joshua.soileau@gmail.com>
2017-03-30 19:09:17 -0400

    Redo entire gallery section - add testimonials

--- build/js/see-all.js
+++ build/js/see-all.js
@@ -4,7 +4,11 @@ jQuery(function($) {
     });
     $(".menu a").click(function() {
         $(".state-visible").removeClass("state-visible"),
-        $(".state-fixed").removeClass("state-fixed"),
-        $(".state-opened").removeClass("state-opened")
+            $(".state-fixed").removeClass("state-fixed"),
+            $(".state-opened").removeClass("state-opened")
+    });
+    $('.gallery-start').click(function() {
+        $(this).closest('.house').find('.works__item__link').first().click();
     });
 });
+
--- src/js/see-all.js
+++ src/js/see-all.js
@@ -7,4 +7,8 @@ jQuery(function($) {
         $(".state-fixed").removeClass("state-fixed"),
         $(".state-opened").removeClass("state-opened")
     });
+    $('.gallery-start').click(function() {
+        $(this).closest('.house').find('.works__item__link').first().click();
+    });
 });
+
